f8e15dc071d373f1370d63cd637ad562e515bfda
Brent Keller <bikeller104@gmail.com>
2023-01-07 17:46:02 -0700

    filled out models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    product_id:{
+      type: DataTypes.INTEGER,
+      reference: {
+        model: 'Product',
+        key: 'id',
+      }
+    },
+    tag_id:{
+      type: DataTypes.INTEGER,
+      reference: {
+        model: 'Tag',
+        key: 'id',
+      }
+    },
   },
   {
     sequelize,
